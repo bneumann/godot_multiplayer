3c7c460dd14e850fa746944945059e7344a932d2
Benjamin Giesinger <benjamin.giesinger@pm.me>
2023-03-03 18:22:14 +0100

    Added multiplayer UI

---
+++ Multiplayer.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class Multiplayer : Node
+{
+	private const int Port = 4433;
+	private Control Ui => GetNode<Control>("UI");
+	private Control RemoteAddressField => GetNode<LineEdit>("UI/Net/Options/Remote");
+
+	public override void _Ready()
+	{
+		// https://godotengine.org/article/multiplayer-in-godot-4-0-scene-replication/
+		// The article sets the state to paused which causes the callbacks to not being called!
+
+		//GetTree().Paused = true;
+		Multiplayer.Set("server_relay", false);
+
+		if (DisplayServer.GetName() == "headless")
+			OnConnectPressed();		
+	}
+
+	public void OnHostPressed()
+	{
+		var peer = new ENetMultiplayerPeer();
+		peer.CreateServer(Port);
+		if (peer.GetConnectionStatus() == MultiplayerPeer.ConnectionStatus.Disconnected)
+		{
+			OS.Alert("Failed to start multiplayer server.");
+			return;
+		}
+
+		Multiplayer.MultiplayerPeer = peer;
+		StartGame();
+	}
+
+	public void OnConnectPressed()
+	{
+		var address = RemoteAddressField.Get("text").AsString();
+		if (string.IsNullOrWhiteSpace(address))
+		{
+			OS.Alert("Need a remote to connect to.");
+			return;
+		}
+
+		var peer = new ENetMultiplayerPeer();
+		peer.CreateClient(address, Port);
+		if (peer.GetConnectionStatus() == MultiplayerPeer.ConnectionStatus.Disconnected)
+		{
+			OS.Alert("Failed to connect to server");
+			return;
+		}
+
+		Multiplayer.MultiplayerPeer = peer;
+		StartGame();
+	}
+	
+	private void StartGame()
+	{
+		Ui.Hide();
+		GetTree().Paused = false;
+	}
+}
